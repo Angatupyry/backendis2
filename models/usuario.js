e56f99042517c13e80a625f3058ab437d595b1e7
Angatupyry <fierrofenix@gmail.com>
2020-03-14 16:24:03 -0300

    Avances en los modelos

--- models/usuario.js
+++ models/usuario.js
@@ -1,6 +1,6 @@
 module.exports = function (sequelize, DataTypes) {
     return sequelize.define('usuario', {
-        id_usuario: {
+        id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
@@ -8,14 +8,54 @@ module.exports = function (sequelize, DataTypes) {
         },
         username: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: true,
+            unique: 'usuario_username_key'
         },
-        pass: {
+        nombre: {
             type: DataTypes.STRING,
             allowNull: false
+        },
+        apellido: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        fecha_alta: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: sequelize.fn('now')
+        },
+        activo: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+            defaultValue: false
+        },
+        proyecto_ud: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'proyecto',
+                key: 'id'
+            }
+        },
+        rol_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'rol',
+                key: 'id'
+            }
         }
+
     }, {
-        tableName: 'is_usuario',
+        tableName: 'usuario',
         timestamps: false,
         schema: 'public'
     });
