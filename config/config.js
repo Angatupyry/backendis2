6df50d9991761f6b434d7ca584cc217b441291bf
Angatupyry <fierrofenix@gmail.com>
2020-03-18 23:29:47 -0300

    fix port

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     api: {
-        port: process.env.API_PORT || 3000,
+        port: process.env.PORT || 3000,
         algoritm: process.env.BEP_ALG || 'HS512',
         secret: process.env.BEP_SECRET || '!laclave!',
         corsOptions: {
