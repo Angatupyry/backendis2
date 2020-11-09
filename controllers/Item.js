dbcc9fb0e8d1df53100d87c10495e9a6f67801dd
Angatupyry <fierrofenix@gmail.com>
2020-11-09 15:06:31 -0300

    Fixes varios

--- controllers/Item.js
+++ controllers/Item.js
@@ -1,3 +1,4 @@
+const { restart } = require('nodemon')
 const {
     item,
     estado
@@ -6,7 +7,26 @@ module.exports = {
     async create(req, res, next) {
         try {
             let newItem
-            
+
+            // const estadoFinalizado = await estado.findOne({
+            //     where: {
+            //         nombre_tabla: 'Proyecto',
+            //         descripcion: 'Finalizado'
+            //     }
+            // })
+
+            // const proyecto = await proyecto.findOne({
+            //     where: {
+            //         id: req.params.proyecto_id,
+            //         estado_id: !estadoFinalizado.id
+            //     }
+            // })
+
+
+            // if (!proyecto.length) {
+            //     return res.status(200).json({})
+            // }
+
             const estadoIniciado = await estado.findOne({
                 where: {
                     nombre_tabla: 'Item',
