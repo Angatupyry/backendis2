3aecf0ff91ee74ffa4994747e5d2d1321cb3a30a
Angatupyry <fierrofenix@gmail.com>
2020-11-09 15:37:44 -0300

    Fixs

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use(xPoweredBy)
 
 
 function xPoweredBy(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   res.header('X-Powered-By', '')
   res.header('X-Hello-Human', '')
   next()
